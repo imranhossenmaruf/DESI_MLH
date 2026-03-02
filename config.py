5090579ab2b49c784d7d4909ef1bf6485357f355
imranhossenmaruf <iimranhossenmaruf@gmail.com>
2026-03-02 20:52:28 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv() # .env ফাইল থেকে ডাটা লোড করার জন্য
+# .env ফাইল থেকে ডাটা লোড করা
+load_dotenv()
 
 API_ID = int(os.getenv("API_ID", "0"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(id) for id in os.getenv("ADMIN_IDS", "").split(",") if id]
+# ADMIN_IDS কমা দিয়ে আলাদা করা থাকলে সেটি লিস্টে রুপান্তর
+ADMIN_IDS = [int(i) for i in os.getenv("ADMIN_IDS", "").split(",") if i]
 DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "0"))
 MONGO_URL = os.getenv("MONGO_URL")
