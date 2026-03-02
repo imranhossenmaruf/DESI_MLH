25085ea02b055852db44173e7eb56011fdc1b321
imranhossenmaruf <iimranhossenmaruf@gmail.com>
2026-03-02 16:55:10 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,8 @@ API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 MONGO_URL = os.environ.get("MONGO_URL")
 
-ADMIN_IDS = list(map(int, os.environ.get("ADMIN_IDS").split(",")))
-LOG_GROUP_ID = int(os.environ.get("LOG_GROUP_ID"))
+ADMIN_IDS = []
+if os.environ.get("ADMIN_IDS"):
+    ADMIN_IDS = list(map(int, os.environ.get("ADMIN_IDS").split(",")))
+
+LOG_GROUP_ID = int(os.environ.get("LOG_GROUP_ID", 0))
