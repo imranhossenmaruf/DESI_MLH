a60c96d887233d41d380f6c16dd581de71113d47
imranhossenmaruf <iimranhossenmaruf@gmail.com>
2026-03-02 16:36:20 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+MONGO_URL = os.environ.get("MONGO_URL")
+
+ADMIN_IDS = list(map(int, os.environ.get("ADMIN_IDS").split(",")))
+LOG_GROUP_ID = int(os.environ.get("LOG_GROUP_ID"))
