a91e668db6a6e5e2640421f38c18fe5001b1ff45
imranhossenmaruf <iimranhossenmaruf@gmail.com>
2026-03-04 00:20:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ async def admin_mode_off(client, message):
     )
 
 # ৬. ব্রডকাস্ট উইজার্ড
-@app.on_message(filters.private & filters.user(ADMIN_IDS) & ~filters.command(["start", "admin", "adminexit", "video", "profile", "overview", "overview7days", "broadcast", "broadcastoff"]))
+@app.on_message(filters.private & filters.user(ADMIN_IDS) & ~filters.command(["start", "admin", "adminexit", "video", "profile", "overview", "overview7days", "broadcast", "broadcastoff"]) & ~filters.regex(r"\|"))
 async def broadcast_wizard(client, message):
     if message.from_user.id in ADMIN_MODE_USERS:
         user_id = message.from_user.id
