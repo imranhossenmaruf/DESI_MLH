062d5ed77644d16f2f4095459db5fdf3dcfa5d4b
imranhossenmaruf <iimranhossenmaruf@gmail.com>
2026-03-08 12:31:31 +0600

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters
+from messages.start.start_msg import start_message
+from buttons.start.start_buttons import start_buttons
+
+
+@Client.on_message(filters.command("start"))
+async def start_command(client, message):
+
+    text = start_message(message.from_user)
+    buttons = start_buttons()
+
+    await message.reply(
+        text,
+        reply_markup=buttons
+    )
