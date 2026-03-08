24fbb14bd1bb22a19baf10e8567282b26d7e0751
imranhossenmaruf <iimranhossenmaruf@gmail.com>
2026-03-08 13:19:08 +0600

    Create buttons/start/start_buttons.py

---
+++ buttons/start/start_buttons.py
@@ -0,0 +1,37 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import BOT_USERNAME
+
+start_buttons = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                "➕ Add Me To Group",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "💎 VIP",
+                url="https://t.me/YOUR_CHANNEL_LINK"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "📊 MY STATUS",
+                callback_data="my_status"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "💰 BUY PREMIUM",
+                url="https://t.me/YOUR_USERNAME?text=I%20want%20to%20buy%20premium%20access%20of%20your%20bot"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "🏆 LEADERBOARD",
+                callback_data="leaderboard"
+            )
+        ]
+    ]
+)
